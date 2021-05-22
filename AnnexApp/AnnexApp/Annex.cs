9999322623d33e6e453335e2042090d6297debbf
Fl33x0 <oleg-voda@mail.ru>
2021-05-22 15:49:09 +0400

    Initial commit

---
+++ AnnexApp/AnnexApp/Annex.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace AnnexApp
+{
+    public struct Annex
+    {
+        private string _basicWord;
+        private char? _literal;
+
+        public Annex(string s)
+        {
+            string _baseWordRegExp = @"^Приложение ";
+            string _literalsRegExp = @"[А-ЖИК-НП-ЦШЩЭЮЯA-HJN0-9]$";
+            Regex _r = new Regex(_baseWordRegExp + _literalsRegExp);
+
+            if (_r.IsMatch(s))
+            {
+                _basicWord = "Приложение ";
+                _literal = s.ToCharArray()[11];
+            }
+            else
+            {
+                _basicWord = null;
+                _literal = null;
+                throw new Exception("Неверная строка для создания экземпляра приложения");
+            }
+        }
+
+        public override string ToString()
+        {
+            return _basicWord + _literal;
+        }
+
+        public Annex GetNextAnnex()
+        {
+            Annex _newAnnex = new Annex();
+            _newAnnex._basicWord = "Приложение ";
+
+            string _requiredChars = "АБВГДЕЖИКЛМНПРСТУФХЦШЩЭЮЯABCDEFGHJKLMNPQRSTUVWXYZ0123456789";
+            char[] _literals = _requiredChars.ToCharArray();
+
+            int _currentLiteralNumber = Array.IndexOf(_literals, _literal);
+
+            _newAnnex._literal = _literals[_currentLiteralNumber + 1];
+
+            return _newAnnex;
+        }
+    }
+}
---
+++ AnnexApp/AnnexApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace AnnexApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Приложение А";
+
+            Annex a = new Annex(s);
+            Console.WriteLine("Результат метода Annex.ToString(): " + a.ToString());
+
+            Console.WriteLine("");
+
+            Annex b = a.GetNextAnnex();
+            Console.WriteLine("Результат метода Annex.GetNextAnnex(): " + b.ToString());
+        }
+    }
+}
